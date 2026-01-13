4531ab9197b14360de1bc708f415e1029a84cc02
Corentin <corentin.courtine@epitech.eu>
2026-01-13 10:38:36 +0100

    Update : Experiences and formations javascript done

--- cv.php
+++ cv.php
@@ -8,7 +8,6 @@
         <link rel="stylesheet" href="">
     </head>
     <body>
-        Hello
         <div id="form" class="form">
             <form action="">
             <div class="info">
@@ -23,30 +22,29 @@
             <div id="comp" class="competences">
                 <h1>Compétences :</h1>
                 <input class="comp" type="text" id="comp-input" required>
-                <button type="button" id="comp-button" onclick="addText()">Ajouter</button>
+                <button type="button" id="comp-button" onclick="addComp()">Ajouter</button>
             </div>
             <div id="experiences" class="exp">
                 <h1>Expériences professionelles :</h1>
-                <input class="nom-poste" type="text">
-                <input class="nom-entreprise" type="text">
-                <input class="exp-date" type="text">
-                <input class="exp-date-fin" type="text">
+                <input class="nom-poste" id="nom-poste" type="text">
+                <input class="nom-entreprise" id="nom-entreprise" type="text">
+                <input class="exp-date" id="exp-date" type="text">
+                <input class="exp-date-fin" id="exp-date-fin" type="text">
                 <textarea name="exp" id="desc-exp"></textarea>
-                <button type="button" >Ajouter</button>
+                <button id="exp-button" type="button" onclick="addExp()" >Ajouter</button>
             </div>
-            <div class="formations">
+            <div class="formations" id="formations">
                 <h1>Formations :</h1>
-                <input class="nom-formations" type="text">
-                <input class="etab" type="text">
-                <input class="form-date" type="text">
-                <input class="form-date-fin" type="text">
-                <textarea class="desc-form" name="forma" id="desc-formation"></textarea>
-                <button type="button" >Ajouter</button>
+                <input class="nom-formations" id="nom-formations" type="text">
+                <input class="etab" id="etab" type="text">
+                <input class="form-date" id="form-date" type="text">
+                <input class="form-date-fin" id="form-date-fin" type="text">
+                <textarea class="desc-form" id="desc-form" name="forma" id="desc-formation"></textarea>
+                <button type="button" onclick="addForm()" >Ajouter</button>
             </div>
             <button type="submit">Valider</button>
             </form>
         </div>
-
         <script src="main.js"></script>
     </body>
 </html>
--- main.js
+++ main.js
@@ -1,12 +1,88 @@
-Input = document.getElementById("comp-input")
+
 button = document.getElementById("comp-button")
 
-function addText() {
-    
+function addComp() {
+    Input = document.getElementById("comp-input")
     newContent = document.createTextNode(Input.value);
     parentDiv = document.getElementById("comp");
     newDiv = document.createElement("p");
     newDiv.classList.add("comp-list")
     newDiv.appendChild(newContent);
+    parentDiv.appendChild(newDiv)
+}
+
+function addExp() {
+    parentDiv = document.getElementById("experiences")
+    newDiv = document.createElement("div")
+    newDiv.classList.add("exp-list")
+
+    Input = document.getElementById("nom-poste")
+    newItem = document.createElement("h2")
+    newContent = document.createTextNode(Input.value)
+    newItem.appendChild(newContent)
+    newDiv.appendChild(newItem)
+
+    Input = document.getElementById("nom-entreprise")
+    newItem = document.createElement("h3")
+    newContent = document.createTextNode(Input.value)
+    newItem.appendChild(newContent)
+    newDiv.appendChild(newItem)
+
+    Input = document.getElementById("exp-date")
+    newItem = document.createElement("h3")
+    newContent = document.createTextNode(Input.value)
+    newItem.appendChild(newContent)
+    newDiv.appendChild(newItem)
+
+    Input = document.getElementById("exp-date-fin")
+    newItem = document.createElement("h3")
+    newContent = document.createTextNode(Input.value)
+    newItem.appendChild(newContent)
+    newDiv.appendChild(newItem)
+
+    Input = document.getElementById("desc-exp")
+    newItem = document.createElement("p")
+    newContent = document.createTextNode(Input.value)
+    newItem.appendChild(newContent)
+    newDiv.appendChild(newItem)
+
+    parentDiv.appendChild(newDiv)
+}
+
+function addForm() {
+    parentDiv = document.getElementById("formations")
+    newDiv = document.createElement("div")
+    newDiv.classList.add("form-list")
+
+    Input = document.getElementById("nom-formations")
+    newItem = document.createElement("h2")
+    newContent = document.createTextNode(Input.value)
+    newItem.appendChild(newContent)
+    newDiv.appendChild(newItem)
+
+    Input = document.getElementById("etab")
+    newItem = document.createElement("h3")
+    newContent = document.createTextNode(Input.value)
+    newItem.appendChild(newContent)
+    newDiv.appendChild(newItem)
+
+    Input = document.getElementById("form-date")
+    newItem = document.createElement("h3")
+    newContent = document.createTextNode(Input.value)
+    newItem.appendChild(newContent)
+    newDiv.appendChild(newItem)
+
+    Input = document.getElementById("form-date-fin")
+    newItem = document.createElement("h3")
+    newContent = document.createTextNode(Input.value)
+    newItem.appendChild(newContent)
+    newDiv.appendChild(newItem)
+
+    Input = document.getElementById("desc-form")
+    newItem = document.createElement("p")
+    newContent = document.createTextNode(Input.value)
+    newItem.appendChild(newContent)
+    newDiv.appendChild(newItem)
+
     parentDiv.appendChild(newDiv)
 }
